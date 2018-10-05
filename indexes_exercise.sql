4a3f07b270a2ef0760d5451bd8b328e126e97f9d
daniel-browning <daniel.d.browning@gmail.com>
2018-10-05 15:45:41 -0500

    mysql indexes exercise;

---
+++ indexes_exercise.sql
@@ -0,0 +1,16 @@
+use employees_db;
+
+describe departments;
+
+describe dept_emp;
+
+describe dept_manager;
+
+describe employees;
+
+describe salaries;
+
+describe titles;
+
+ALTER TABLE albums
+ADD UNIQUE (record_name, artist);
